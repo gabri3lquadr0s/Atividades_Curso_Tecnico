8c8ca3173b17c8825ad1894388e124c9a81441ac
gabri3lquadr0s <gabriel_q_teodoro@estudante.sesisenai.org.br>
2023-05-05 11:25:03 -0300

    Po mano tem mais alteração

--- Lógica de Programação/Primeiro Trimestre/JavaScript-HTML/Conversão de Temperaturas/index.js
+++ Lógica de Programação/Primeiro Trimestre/JavaScript-HTML/Conversão de Temperaturas/index.js
@@ -1,10 +1,16 @@
 function converte(){
+    //var temperatura é pega do input com id tempValor
     var temperatura = Number(document.getElementById("tempValor").value)
     var temperaturaConvertida
+    //Aqui, o código checa se o botão radius de um id específico está marcado
     if(document.getElementById("fc").checked){
+        //A var temperaturaConvertida é definida pela conta de converção associada com cada tipo de medição
         temperaturaConvertida = (temperatura - 32)*5/9
+        //No elemento HTML com o id resultado, o valor de temperaturaConvertida é mostrado, arredondado para
+        //dois dígitos com tofixed(2) e também com o respectivo símbolo de medição
         document.getElementById("resultado").innerHTML = (`${temperaturaConvertida.toFixed(2)} C°`)
     }
+    //Se a afirmação não foi verdadeira, ele passa para a próxima checagem
     else if(document.getElementById("cf").checked){
         temperaturaConvertida = temperatura*9/5+32
         document.getElementById("resultado").innerHTML = (`${temperaturaConvertida.toFixed(2)} F°`)
