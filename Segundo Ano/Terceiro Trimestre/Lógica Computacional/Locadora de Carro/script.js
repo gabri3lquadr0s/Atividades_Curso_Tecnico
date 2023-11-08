74f72eb9efabd25190a15cc7497a99ee00a20deb
Gabriel de Quadros <gabrielquadrosrodapiao@gmail.com>
2023-11-08 17:08:10 -0300

    !fix: newCar && getCars, !add rentCar

--- Segundo Ano/Terceiro Trimestre/Lógica Computacional/Locadora de Carro/script.js
+++ Segundo Ano/Terceiro Trimestre/Lógica Computacional/Locadora de Carro/script.js
@@ -1,4 +1,12 @@
-var carList = [{"plate":"asd123", "year":1900, "brand":"ford", "color":"0066FF", "model":"ka", "isRented":false, "renter":""}, {"plate":"fgh123", "year":1920, "brand":"wolksvager", "color":"1236FF", "model":"gol", "isRented":true, "renter":"gabriel"}];
+/*
+        ----TODO---- 
+ADD VALIDATION FOR CAR INFORMATION SENT TO SCRIPT
+SAVE INFORMATION IN JSON FILE FOR PERMANENT DATA STORAGE(USE BROWSEFIY LIB)
+FIX ISSUE OF MULTIPLE DATA BEING SENT AT ONCE
+        ------------
+*/
+
+var carList = [{"plate":"fgh123", "year":1920, "brand":"wolksvager", "color":"1236FF", "model":"gol", "isRented":true, "renter":"gabriel"},{"plate":"123456", "year":1927, "brand":"wolksvager", "color":"5556FF", "model":"gol", "isRented":false, "renter":""} ,{"plate":"asd123", "year":1900, "brand":"ford", "color":"0066FF", "model":"ka", "isRented":false, "renter":""}];
 
 function newCar() {
     let car = new Object();
@@ -10,43 +18,77 @@ function newCar() {
         car.brand = document.getElementById('brand').value;
         car.color = document.getElementById('color').value;
         car.model = document.getElementById('model').value;
-        if(document.getElementById('renter').value != '') {
-            car.isRented = true;
-            car.renter = document.getElementById('renter').value;
-        } else {
-            car.isRented = false;
-            car.renter = '';
-        }
+        car.isRented = false;
+        car.renter = '';
         console.log(car);
         carList.push(car);
     })
     return true;
 }
 
-function rentCar() {
-    let form = document.getElementById('registerRenterForm');
-    let renter = document.getElementById('renterName');
-    //TODO rentCar() logic
-}
+/*    
+        ----TODO---- 
+ADD getCarByRent LOGIC ON FUNCTION
+RESOLVE OBJ RETURN STYLE TO HTML
+        ------------
+*/
 
 function getCars() {
-    let plateSent = document.getElementById('getPlate').value;
-    let list = document.getElementById('list')
-    if(plateSent) {
-        let result = carList.filter(obj => {
-            if(obj.plate === plateSent) {
-                return list.innerHTML = `<li>${JSON.stringify(obj)}</li>`;
-            } else {
-                return list.innerHTML = `<h1>${plateSent} not found</h1>`
-            }
-        })
-    } else {
-        for(var i = 0; i < carList.length; i++) {
-            let listElement = document.createElement('li');
-            listElement.innerText = JSON.stringify(carList[i]);
-            list.appendChild(listElement);
+    let form = document.getElementById('carListForm');
+    form.addEventListener('submit',(e) => {
+        e.preventDefault();
+        let plateSent = document.getElementById('getPlate').value;
+        let list = document.getElementById('list');
+        list.innerHTML = '';
+        if(plateSent) {
+            let arr = [];
+            let result = carList.filter(obj => {
+                if(obj.plate == plateSent || obj.model == plateSent || obj.brand == plateSent) arr.push(obj);
+            })
+            if(arr.length === 0) return list.innerHTML = `<h3>${plateSent} not found</h3>`;
+
+            for(var i = 0; i < arr.length; i++) {
+                let listElement = document.createElement('li');
+                listElement.innerText = JSON.stringify(arr[i]);
+                list.appendChild(listElement);
+            } return;
+
+        } else {
+            for(var i = 0; i < carList.length; i++) {
+                let listElement = document.createElement('li');
+                listElement.innerText = JSON.stringify(carList[i]);
+                list.appendChild(listElement);
+            } return;
         }
-    }
+    })        
+}
 
 
+/* 
+        ----TODO---- 
+FIX VALIDATION RENT AND UNRENT
+VISUAL VALIDATION FOR ACTION
+CLEAN CODE WITH LESS IF ELSE STATEMENTS
+        ------------
+*/
+
+function rentCar() {
+    let renter = document.getElementById('renter').value;
+    let plateSent = document.getElementById('carToRent').value;
+    let form = document.getElementById('carRentForm');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let result = carList.filter(obj=> {
+            if(obj.plate == plateSent) {
+                if(document.getElementById('rent').checked && obj.isRented == false) {
+                    obj.isRented = true;
+                    obj.renter = renter;
+                    console.log(obj);
+                } else if(document.getElementById('unrent').checked) {
+                    obj.isRented = false;
+                    obj.renter = '';
+                }
+            }
+        }) 
+    })
 }
