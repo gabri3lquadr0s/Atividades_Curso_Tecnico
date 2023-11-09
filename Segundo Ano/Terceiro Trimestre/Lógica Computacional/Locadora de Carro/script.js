0eb59b934a0b3ec47ddc1c014c1eb104d1e8dee7
gabri3lquadr0s <gabrielquadrosrodapiao@gmail.com>
2023-11-09 20:40:13 -0300

    feature: add getCarByRent on getCars()

--- Segundo Ano/Terceiro Trimestre/Lógica Computacional/Locadora de Carro/script.js
+++ Segundo Ano/Terceiro Trimestre/Lógica Computacional/Locadora de Carro/script.js
@@ -8,27 +8,38 @@ FIX ISSUE OF MULTIPLE DATA BEING SENT AT ONCE
 
 var carList = [{"plate":"fgh123", "year":1920, "brand":"wolksvager", "color":"1236FF", "model":"gol", "isRented":true, "renter":"gabriel"},{"plate":"123456", "year":1927, "brand":"wolksvager", "color":"5556FF", "model":"gol", "isRented":false, "renter":""} ,{"plate":"asd123", "year":1900, "brand":"ford", "color":"0066FF", "model":"ka", "isRented":false, "renter":""}];
 
+function Car(plate, year, brand, color, model, isRented, renter) {
+    this.plate = plate;
+    this.year = year;
+    this.brand = brand;
+    this.color = color;
+    this.model = model;
+    this.isRented = isRented;
+    this.renter = renter;
+}
+
 function newCar() {
-    let car = new Object();
     let form = document.getElementById('registerCarForm');
     form.addEventListener('submit',(e) => {
         e.preventDefault();
-        car.plate = document.getElementById('plate').value;
-        car.year = document.getElementById('year').value;
-        car.brand = document.getElementById('brand').value;
-        car.color = document.getElementById('color').value;
-        car.model = document.getElementById('model').value;
-        car.isRented = false;
-        car.renter = '';
-        console.log(car);
-        carList.push(car);
+        let newCar = new Car(
+            document.getElementById('plate').value,
+            document.getElementById('year').value,
+            document.getElementById('brand').value,
+            document.getElementById('color').value,
+            document.getElementById('model').value,
+            false,
+            ''
+        );
+        console.log(newCar);
+        carList.push(newCar);
+        console.log(carList);
     })
     return true;
 }
 
 /*    
         ----TODO---- 
-ADD getCarByRent LOGIC ON FUNCTION
 RESOLVE OBJ RETURN STYLE TO HTML
         ------------
 */
@@ -39,14 +50,15 @@ function getCars() {
         e.preventDefault();
         let plateSent = document.getElementById('getPlate').value;
         let list = document.getElementById('list');
+        let message = document.getElementById('message');
+        message.innerHTML = '';
         list.innerHTML = '';
+        let arr = [];
         if(plateSent) {
-            let arr = [];
             let result = carList.filter(obj => {
                 if(obj.plate == plateSent || obj.model == plateSent || obj.brand == plateSent) arr.push(obj);
             })
-            if(arr.length === 0) return list.innerHTML = `<h3>${plateSent} not found</h3>`;
-
+            if(arr.length == 0)  return message.innerHTML = `${plateSent} not found`;
             for(var i = 0; i < arr.length; i++) {
                 let listElement = document.createElement('li');
                 listElement.innerText = JSON.stringify(arr[i]);
@@ -54,21 +66,40 @@ function getCars() {
             } return;
 
         } else {
+            if(document.getElementById('getCarByRent').checked) {
+                let result = carList.filter(obj =>{
+                    if(obj.isRented == true) arr.push(obj);
+                }); if(arr.length == 0) return message.innerHTML = `No cars rented in the moment`;
+                for(var i = 0; i < arr.length; i++) {
+                    let listElement = document.createElement('li');
+                    listElement.innerText = JSON.stringify(arr[i]);
+                    list.appendChild(listElement);
+                } document.getElementById('getCarByRent').checked=false; 
+                return;
+            } else if(document.getElementById('getCarByNotRent')) {
+                let result = carList.filter(obj => {
+                    if(obj.isRented == false) arr.push(obj);
+                }); if(arr.length == 0) return message.innerHTML = `No cars free in the moment`;
+                for(var i = 0; i < arr.length; i++) {
+                    let listElement = document.createElement('li');
+                    listElement.innerText = JSON.stringify(carList[i]);
+                    list.appendChild(listElement);
+                } document.getElementById('getCarByNotRent').checked=false; 
+                return;
+            }
             for(var i = 0; i < carList.length; i++) {
                 let listElement = document.createElement('li');
                 listElement.innerText = JSON.stringify(carList[i]);
                 list.appendChild(listElement);
             } return;
-        }
-    })        
+    }
+    });
 }
-
 
 /* 
         ----TODO---- 
 FIX VALIDATION RENT AND UNRENT
 VISUAL VALIDATION FOR ACTION
-CLEAN CODE WITH LESS IF ELSE STATEMENTS
         ------------
 */
 
@@ -88,6 +119,8 @@ function rentCar() {
                     obj.isRented = false;
                     obj.renter = '';
                 }
+            } else {
+                document.getElementById('message1').innerHTML = `${plateSent} not found`;
             }
         }) 
     })
